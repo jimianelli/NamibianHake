0297fa5c1f1d0ec4190fd527c23c00537804bf45
jimianelli <jimianelli@users.noreply.github.com>
2024-07-16 17:15:43 +0000

    Style code (GHA)

--- R/get-results.R
+++ R/get-results.R
@@ -7,26 +7,28 @@
 #' @param moddir The main directory path for the models. Default is 'mod_dir'
 #' @return A list containing model results.
 #' @export
-#mod_names. = mod_label; rundir = "mods"; moddir = mod_dir;
+# mod_names. = mod_label; rundir = "mods"; moddir = mod_dir;
 get_results <- function(mod_names. = mod_names,
                         rundir = "mods",
                         moddir = mod_dir,
                         run_on_mac = TRUE) {
   if (run_on_mac) {
-    fn        <- paste0(rundir, "/", moddir, "/nh_R.rep")
+    fn <- paste0(rundir, "/", moddir, "/nh_R.rep")
   } else {
-    #run on windows
-    fn <- paste0(rundir, "\\", moddir, "\\nh_R.rep") #rundir is "C:\\GitProjects\\EBSpollock\\2023_runs\\"
+    # run on windows
+    fn <- paste0(rundir, "\\", moddir, "\\nh_R.rep") # rundir is "C:\\GitProjects\\EBSpollock\\2023_runs\\"
   }
   nmods <- length(mod_names.)
   num_cores <- parallel::detectCores() - 2
   cl <- parallel::makeCluster(num_cores)
-  on.exit(parallel::stopCluster(cl))  # Ensure the cluster stops after function execution
+  on.exit(parallel::stopCluster(cl)) # Ensure the cluster stops after function execution
 
   # Export necessary functions and objects to the cluster
-  #source("R/read-admb2.R")
-  parallel::clusterExport(cl, c("read_fit", "read_admb", "read_rep"), envir =
-                            environment())
+  # source("R/read-admb2.R")
+  parallel::clusterExport(cl, c("read_fit", "read_admb", "read_rep"),
+    envir =
+      environment()
+  )
 
   # Fetch model results in parallel
   system.time(modlst <- parallel::parLapply(cl = cl, X = fn, fun = read_rep))
